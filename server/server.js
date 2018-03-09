46e1e4a7aeac06bc070d5573a02420700ae780a3
juce020388 <bloodclaymore@gmail.com>
2018-03-09 09:30:27 +0200

    3rd try to use heroku

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -32,24 +31,26 @@ app.get('/todos', (req, res) => {
   });
 });
 
-app.get('/todos/:id',(req, res) => {
-  let id = req.params.id;
-  if(!ObjectID.isValid(id)) {
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
+
   Todo.findById(id).then((todo) => {
-    if(!todo) {
+    if (!todo) {
       return res.status(404).send();
     }
+
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
-  })
+  });
+});
 
-})
-
-app.listen(port, (port) => {
-  console.log(`Started on port ${port}`);
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
